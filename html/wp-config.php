abd153141f868cc3be1f8c9d34f9659932c85f87
Deepak Tiwari <dtiwar@gmail.com>
2019-11-23 18:39:07 +0100

    new

--- html/wp-config.php
+++ html/wp-config.php
@@ -86,5 +86,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
+define( 'UPLOADS', 'wp-content/uploads' );
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
